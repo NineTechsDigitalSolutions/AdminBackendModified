e7b5066e6857c453551a0070a72ca97c457da1b1
Navod Kamilka <navod@tsdclanka.com>
2023-09-20 20:23:28 +0530

    add files

--- routes/author.js
+++ routes/author.js
@@ -2,14 +2,16 @@ const express = require("express");
 
 const authorController = require("../controllers/author");
 const { uploadImages } = require("../middleware/uploadImage");
+const userAuth = require("../middleware/userAuth")
 const router = express.Router();
 
-router.get("/get-all", authorController.getAll);
+router.get("/get-all",userAuth, authorController.getAll);
 // router.get("/get-by-library/:id", authorController.getAllByLibrary);
-router.get("/get-author/:id", authorController.getAuthor);
+router.get("/get-author/:id",userAuth, authorController.getAuthor);
 
-router.post("/create", uploadImages, authorController.createAuthor);
-router.post("/update", uploadImages, authorController.editAuthor);
-router.post("/search", authorController.searchAuthors);
+//router.post("/create",userAuth, uploadImages, authorController.createAuthor);
+router.post("/create",userAuth, uploadImages, authorController.createAuthor);
+router.post("/update",userAuth, uploadImages, authorController.editAuthor);
+router.post("/search",userAuth, authorController.searchAuthors);
 
 module.exports = router;
